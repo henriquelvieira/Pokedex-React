3f41e1cc2ba5a60bcf8e03f45c728861188aa2b7
Henrique Lopes <henrique.l.vieira92@gmail.com>
2021-07-07 20:09:15 -0300

    Trabalhando no recurso de sugestão da busca

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import { useDebounce } from '../../hooks/useDebounce';
 
+import {pokemonsList} from './pokemonsList';
+
 function SearchBar({value, onChange}) {
 
 
     const [displayValue, setDisplayValue]   = useState(value);
-    const debouncedChange = useDebounce(onChange, 500);
+    const debouncedChange = useDebounce(onChange, 700);
 
     function handleChange(event) {
         const value = event.target.value;
@@ -24,7 +26,16 @@ function SearchBar({value, onChange}) {
                             placeholder="Pesquisar Pokémon (Nome ou ID)..." 
                             value={displayValue} 
                             onChange={handleChange} 
+                            list="browsers"
                         />
+                        <datalist id="browsers">
+                            {pokemonsList.map(dados => (
+                                <option value={dados.name} />
+                            )
+                            
+                            )};
+ 
+                        </datalist>
                     </div>
                 </div>
             </section>
